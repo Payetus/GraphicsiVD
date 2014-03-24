ebc12a7175cff64767ba5b039719f843128410dc
CRISTIAN ROBERT PAYET WALSH <crpayetw7.alumnes@ub052028>
2014-03-24 17:06:35 +0100

    AComments per la practica 2

--- Practiques/Practica1/Carreres/vshader11.glsl
+++ Practiques/Practica1/Carreres/vshader11.glsl
@@ -11,11 +11,13 @@ IN vec4 vPosition;
 IN vec4 vColor;
 
 OUT vec4 color;
-
-
-
+//Enviar la matriu a la GPU
+//Aixo es fa desde camera
+//Es te que afegir la matriu
+//uniform mat4 model.view;
 void main() 
 {
+  //Es te que multiplicar  Uniform
   gl_Position = vPosition;
   color = vColor;
 } 
